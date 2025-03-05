05f25821588f37f07c92ae3af0ccb5bd26550a22
student-a502 <student-a502@PC-A502-18>
2025-03-05 09:46:23 +0500

    Реализовал класс MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private List<Note> noteList;
+    private NoteAdapter noteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        noteList = new ArrayList<>();
+        RecyclerView recyclerViewNotes = findViewById(R.id.recycle);
+        recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(noteList);
+        recyclerViewNotes.setAdapter(noteAdapter);
+        Button buttonAddNote = findViewById(R.id.sozd);
+        buttonAddNote.setOnClickListener(v ->{
+            Intent intent = new Intent(MainActivity.this.AddNoteActivity.class);
+            startActivityForResult(intent,1);
+        });
+        Button buttonDelete=findViewById(R.id.delete);
+        buttonDelete.setOnClickListener(v -> {
+            noteAdapter.deleteSelectedNotes();
         });
     }
+    @Override
+    protected void OnActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==1 & resultCode==-1){
+            String noteContent = data.getStringExtra("note");
+            String currentDate = new SimpleDateFormat("dd.MM.yy", Locale.getDefault()).format(new Date());
+            noteList.add(new Note(currentDate,noteContent));
+            noteAdapter.notifyDataSetChanged();
+        }
+    }
+
 }
